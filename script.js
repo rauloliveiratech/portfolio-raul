aa3e123f167bcb9b60f6a7af014bd718f894bba4
rauloliveiratech <rauloliveiratech@gmail.com>
2023-10-08 21:15:49 -0300

    v1

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('.cabeçalho-link a');
+    links.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1); // Remove o "#" do href
+            scrollToSection(targetId);
+        });
+    });
+
+    function scrollToSection(id) {
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
+    const typed = new Typed("span", {
+        strings: ["Raul Oliveira.", "Web Developer.", "Front-end Developer."],
+        typeSpeed: 70,
+        backSpeed: 60,
+        loop: true
+    });
+});
